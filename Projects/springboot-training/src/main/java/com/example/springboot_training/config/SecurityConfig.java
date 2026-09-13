76db4cf66f5f92b22c414b07e37352585bcd36f9
Aniket Parekar <you@example.com>
2026-09-13 12:39:17 +0530

    Day 5 : Added JWT Security and manager authentication

--- Projects/springboot-training/src/main/java/com/example/springboot_training/config/SecurityConfig.java
+++ Projects/springboot-training/src/main/java/com/example/springboot_training/config/SecurityConfig.java
@@ -2,15 +2,18 @@ package com.example.springboot_training.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
-import com.example.springboot_training.security.JwtAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.springboot_training.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -29,6 +32,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration config) throws Exception {
+
         return config.getAuthenticationManager();
     }
 
@@ -38,14 +42,47 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS))
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
+
+                        // Public endpoints
                         .requestMatchers(
                                 "/auth/register",
                                 "/auth/login")
                         .permitAll()
+
+                        // GET → STAFF + MANAGER
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/staff",
+                                "/api/staff/**")
+                        .hasAnyRole("STAFF", "MANAGER")
+
+                        // POST → MANAGER only
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/api/staff")
+                        .hasRole("MANAGER")
+
+                        // PUT → MANAGER only
+                        .requestMatchers(
+                                HttpMethod.PUT,
+                                "/api/staff/**")
+                        .hasRole("MANAGER")
+
+                        // DELETE → MANAGER only
+                        .requestMatchers(
+                                HttpMethod.DELETE,
+                                "/api/staff/**")
+                        .hasRole("MANAGER")
+
+                        // Other requests
                         .anyRequest().authenticated())
+
                 .addFilterBefore(
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class);
